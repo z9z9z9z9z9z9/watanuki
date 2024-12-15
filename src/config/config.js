cf12592b27d81e66ea596181c986fd8aa3da5651
yahya momin <yahyamomin06@gmail.com>
2024-12-15 17:00:58 +0530

    update new api url

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,6 @@
 const config = {
-  serverUrl: "https://hianime-api-production.up.railway.app/api/v1",
+  // serverUrl: "https://hianime-api-production.up.railway.app/api/v1",
+  serverUrl: "https://hianime-api-ygij.onrender.com/api/v1",
   localUrl: "http://localhost:3030/api/v1",
 };
 
